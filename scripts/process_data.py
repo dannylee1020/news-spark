1ad70dd43ca4212d7b29ca2a134ab211c8b323f0
dannylee1020 <dannylee1020@gmail.com>
2021-02-28 12:45:12 -0800

    flatteneing nested df pyspark

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, explode
 from pyspark.sql.types import StructType, StructField, StringType
 import config
 
@@ -10,31 +10,10 @@ spark.conf.set('spark.sql.shuffle.partitions', 5)
 btc_df = spark.read.format('json').option('inferSchema','true').load(f"{config.PROJECT_HOME}/files/btc_data.json")
 eth_df = spark.read.format('json').option('inferSchema','true').load(f"{config.PROJECT_HOME}/files/eth_data.json")
 
-
-# elements_schema = StructType(
-#     [
-#         StructField('author', StringType, True),
-#         StructField('content', StringType, True),
-#         StructField('description', StringType, True),
-#         StructField('publisedAt', StringType, True),
-#         StructField('source', StructType([
-#             StructField('id', StringType, True),
-#             StructField('name', StringType, True)
-#         ]),
-#         StructField('title', StringtType, True),
-#         StructField('url', StringType, True),
-#         StructField('urlToImage', StringType, True)
-#     ]
-# )
-
-# btc_elements_df = btc_df.select(col('articles.element.*'))
-# eth_elements_df = eth_df.select(col('articles.element.*'))
+explode_articles_df = btc_df.withColumn('articles',explode('articles')).select('*', col('articles.*')).drop(col('articles'))
+flatten_source_df = explode_articles.select('*', 'source.*').drop(col('source'))
 
 
 
 if __name__ == '__main__':
-    btc_df.printSchema()
-    eth_df.printSchema()
-
-    # btc_elements_df.printSchema()
-    # eth_elements_df.printSchema()
+    flatten_source_df.show(10)
