3d15db1b4780b95e04d378d5c720c4719f516d6b
dannylee1020 <dannylee1020@gmail.com>
2021-03-02 11:22:28 -0800

    WIP on processing with spark

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -11,6 +11,9 @@ spark.conf.set('spark.sql.shuffle.partitions', 5)
 btc_news_df = spark.read.format('json').option('inferSchema','true').load(f"{config.PROJECT_HOME}/raw_data/news/btc_news.json")
 eth_news_df = spark.read.format('json').option('inferSchema','true').load(f"{config.PROJECT_HOME}/raw_data/news/eth_news.json")
 
+explode_articles_df = btc_news_df.withColumn('articles',explode('articles')).select('*', col('articles.*')).drop(col('articles'))
+flatten_source_df = explode_articles_df.select('*', 'source.*').drop(*['source','status','totalResults'])
+
 # read price data
 price_schema = StructType([
     StructField('time', LongType(), True),
@@ -21,31 +24,10 @@ price_schema = StructType([
     StructField('volume', FloatType(), True)
 ])
 
-btc_price_txt = sc.textFile(f"{config.PROJECT_HOME}/raw_data/price/btc_price")
-btc_price_data = btc_price_txt.collect()
-
-# df = spark.createDataFrame(btc_price_data, schema=price_schema)
-# btc_price_df = spark.read.format('csv').option('schema',price_schema).load(f"{config.PROJECT_HOME}/raw_data/price/btc_price")
-# btc_price_txt = btc_price_txt.toDF()
-
-# R = Row('time','low','high','open','close','volume')
-# df = spark.createDataFrame(R(x) for x in btc_price_txt)
+btc_price_df = spark.read.format('csv').schema(price_schema).load(f"{config.PROJECT_HOME}/raw_data/price/btc_price.csv")
+eth_price_df = spark.read.format('csv').schema(price_schema).load(f"{config.PROJECT_HOME}/raw_data/price/eth_price.csv")
 
 
-btc_price_txt = spark.read.text(f"{config.PROJECT_HOME}/raw_data/price/btc_price")
-
-
-# df = spark.createDataFrame((x for x in btc_price_txt), schema = price_schema)
-
-# eth_price_df = spark.read.load(f"{config.PROJECT_HOME}/raw_data/price/eth_price")
-
-
-
-
-
-
-explode_articles_df = btc_news_df.withColumn('articles',explode('articles')).select('*', col('articles.*')).drop(col('articles'))
-flatten_source_df = explode_articles_df.select('*', 'source.*').drop(*['source','status','totalResults'])
 
 
 
@@ -62,11 +44,7 @@ flatten_source_df = explode_articles_df.select('*', 'source.*').drop(*['source',
 
 if __name__ == '__main__':
     # flatten_source_df.show(10)
-    # btc_price_txt.printSchema()
-    # temp_df.show(10)
-    # df.show(10)
-    # ast.literal_eval(btc_price_data)
-    # df.show(10)
+    btc_price_df.show(10)
 
     
 
