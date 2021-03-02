d0d5e4d81a7d037ab957480f6c3d3a6539258386
dannylee1020 <dannylee1020@gmail.com>
2021-03-01 21:37:53 -0800

    script for api

--- scripts/api.py
+++ scripts/api.py
@@ -1,8 +1,52 @@
-import json, requests
+import json, requests, hmac, hashlib, time, base64
 from urllib.parse import urlencode
 from requests.auth import AuthBase
 from dotenv import load_dotenv
 import os
+
+class CoinbaseExchangeAuth(AuthBase):
+    def __init__(self, api_key, secret_key, passphrase):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.passphrase = passphrase
+
+    def __call__(self, request):
+        timestamp = str(time.time())
+        message = timestamp + request.method + request.path_url + (request.body or '')
+        hmac_key = base64.b64decode(self.secret_key)
+        signature = hmac.new(hmac_key, message.encode(), hashlib.sha256)    
+        signature_b64 = base64.b64encode(signature.digest())
+
+        request.headers.update({
+            'CB-ACCESS-SIGN': signature_b64,
+            'CB-ACCESS-TIMESTAMP': timestamp,
+            'CB-ACCESS-KEY': self.api_key,
+            'CB-ACCESS-PASSPHRASE': self.passphrase,
+            'Content-Type': 'application/json'
+        })
+        return request
+
+
+class Coinbase_API(CoinbaseExchangeAuth):
+    def __init__(self, api_key, secret_key, passphrase):
+        super().__init__(api_key, secret_key, passphrase)
+        self.coinbaseAuth = CoinbaseExchangeAuth(self.api_key, self.secret_key, self.passphrase)    
+
+    def get_historical_price(self, query = None, product_id = None, start = None, end = None, granularity = None):
+        auth = self.coinbaseAuth
+        endpoint = f"https://api.pro.coinbase.com/products/{product_id}/candles"
+        if isinstance(query, dict):
+            query_param = urlencode(query)
+        else:
+            query_param = urlencode({'start':start, 'end':end, 'granularity':granularity})
+
+        lookup_url = f"{endpoint}?{query_param}"
+        r = requests.get(lookup_url, auth=auth)
+
+        return r.json()
+
+
+
 
 class News_API(object):
     api_key = None
