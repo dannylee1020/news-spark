c1788feacb1fb5f26fa96e8b27ff3536d3e52eda
dannylee1020 <dannylee1020@gmail.com>
2021-03-01 21:37:59 -0800

    config

--- scripts/config.py
+++ scripts/config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 PROJECT_HOME = os.getenv('PROJECT_HOME')
-
+CB_API_KEY = os.getenv('CB_API_KEY')
+CB_API_SECRET = os.getenv('CB_API_SECRET')
+CB_PW = os.getenv('CB_PW')
