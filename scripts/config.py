8a51c2b7061b7664deb2472c53a8b44ce9aed820
dannylee1020 <dannylee1020@gmail.com>
2021-03-07 12:24:17 -0800

    adding env variables to config

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -8,3 +9,9 @@ PROJECT_HOME = os.getenv('PROJECT_HOME')
 CB_API_KEY = os.getenv('CB_API_KEY')
 CB_API_SECRET = os.getenv('CB_API_SECRET')
 CB_PW = os.getenv('CB_PW')
+SPARK_HOME = os.getenv('SPARK_HOME')
+
+# dynamically set date for api calls
+START_DATE= (datetime.now() - timedelta(days = 27)).strftime('%Y-%m-%d')
+END_DATE = datetime.now().strftime('%Y-%m-%d')
+RAW_DATA_DIR = f"{PROJECT_HOME}/raw_data/"
