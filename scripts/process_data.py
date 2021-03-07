6535a5e0c9412636dbac803200f03177d006c222
dannylee1020 <dannylee1020@gmail.com>
2021-03-07 12:24:59 -0800

    postgres connection with spark

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,50 +1,89 @@
-from pyspark.sql import SparkSession, Row
-from pyspark.sql.functions import col, explode
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, explode, to_timestamp, to_date
 from pyspark.sql.types import StructType, StructField, StringType, LongType, FloatType, ArrayType
 import config
+import os
+
+
 
 spark = SparkSession.builder.master('local[*]').getOrCreate()
-sc = spark.sparkContext
 spark.conf.set('spark.sql.shuffle.partitions', 5)
 
-# read news data
-btc_news_df = spark.read.format('json').option('inferSchema','true').load(f"{config.PROJECT_HOME}/raw_data/news/btc_news.json")
-eth_news_df = spark.read.format('json').option('inferSchema','true').load(f"{config.PROJECT_HOME}/raw_data/news/eth_news.json")
+# setting extraClassPath doesn't work in client mode.  
+# .config('spark.driver.extraClassPath', f"{config.SPARK_HOME}/jars/postgresql-42.2.18.jar")\
+# .config('spark.jars', f"{config.SPARK_HOME}/jars/postgresql-42.2.18.jar")\
 
-explode_articles_df = btc_news_df.withColumn('articles',explode('articles')).select('*', col('articles.*')).drop(col('articles'))
-flatten_source_df = explode_articles_df.select('*', 'source.*').drop(*['source','status','totalResults'])
 
-# read price data
-price_schema = StructType([
+# define schema for price data
+price_schema = StructType([ 
     StructField('time', LongType(), True),
-    StructField('low', FloatType(), True),
+    StructField('low', FloatType(), True),  
     StructField('high', FloatType(), True),
     StructField('open', FloatType(), True),
     StructField('close', FloatType(), True),
     StructField('volume', FloatType(), True)
 ])
 
-btc_price_df = spark.read.format('csv').schema(price_schema).load(f"{config.PROJECT_HOME}/raw_data/price/btc_price.csv")
-eth_price_df = spark.read.format('csv').schema(price_schema).load(f"{config.PROJECT_HOME}/raw_data/price/eth_price.csv")
+
+def spark_read_data(news:bool, path, filename, data_type):
+    if news == True:
+        new_path = f"{path}/news/"
+        df = spark.read.format(data_type).option('inferSchema', 'true').load(new_path+filename)
+    else:
+        new_path = f"{path}/price/"
+        df = spark.read.format(data_type).schema(price_schema).load(new_path)
+
+    return df
+
+
+file_path = config.RAW_DATA_DIR
+
+# read news data
+btc_news_df = spark_read_data(news=True, path=file_path, data_type='json', filename = 'btc_news.json')
+eth_news_df = spark_read_data(news=True, path=file_path, data_type='json', filename = 'eth_news.json')
+
+# flatten news_data
+explode_articles_btc = btc_news_df.withColumn('articles',explode('articles')).select('*', col('articles.*')).drop(col('articles'))
+flattened_btc_news = explode_articles_btc.select('*', 'source.*').drop(*['source','status','totalResults'])
+
+explode_articles_eth = eth_news_df.withColumn('articles', explode('articles')).select('*', col('articles.*')).drop(col('articles'))
+flattened_eth_news = explode_articles_eth.select('*', 'source.*').drop(*['source','status','totalResults'])
+
+# add date column 
+flattened_btc_news = flattened_btc_news.withColumn('published_date', to_date(to_timestamp(col('publishedAt')), 'yyyy-MM-dd'))
+flattened_eth_news = flattened_eth_news.withColumn('published_date', to_date(to_timestamp(col('publishedAt')), 'yyyy-MM-dd'))
 
 
 
+# read price data
+btc_price_df = spark_read_data(news=False, path=file_path, data_type='csv', filename = 'btc_price.csv')
+eth_price_df = spark_read_data(news=False, path=file_path, data_type='csv', filename = 'eth_price.csv')
+
+# add date column
+btc_df = btc_price_df.withColumn('date', to_date(to_timestamp(col('time')),'yyyy-MM-dd')).drop(col('time')) 
+eth_df = eth_price_df.withColumn('date', to_date(to_timestamp(col('time')), 'yyyy-MM-dd')).drop(col('time'))
 
 
+# Postgres connection
+jdbc_url = 'jdbc:postgresql://localhost/news_spark'
+properties = {
+    'user':'dhyungseoklee', 
+    'driver':'org.postgresql.Driver'    
+}
 
-# perform transformations 
-
-
-
-
-
-# write out to files
-# load to Postgres
 
 
 if __name__ == '__main__':
-    # flatten_source_df.show(10)
-    btc_price_df.show(10)
+    btc_df.write.jdbc(url=jdbc_url, table = 'btc_price', mode = 'append', properties = properties)
+    eth_df.write.jdbc(url = jdbc_url, table = 'eth_price', mode = 'append', properties = properties)
+
+    flattened_btc_news.write.jdbc(url = jdbc_url, table = 'btc_news', mode = 'append', properties = properties)
+    flattened_eth_news.write.jdbc(url = jdbc_url, table = 'eth_news', mode = 'append', properties = properties)
+
+
+
+    # spark-submit --driver-class-path /Users/dhyungseoklee/Projects/spark/spark-3.0.1-bin-hadoop2.7/jars/postgresql-42.2.18.jar process_data.py
+
 
     
 
